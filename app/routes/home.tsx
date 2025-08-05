aece49625ec76409a7ae17e086a123ae454cdfab
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2025-08-05 20:43:35 +0100

    base setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return (
+		<div className="flex">
+			<section className="bg-gradient-to-b from-[#0066CC] to-[#00BFFF] w-1/6 h-screen flex flex-col items-center">
+				<h2 className="text-2xl text-white font-semibold my-12">EduTrackr</h2>
+				<div className="text-white text-lg capitalize flex flex-col items-center list-none gap-6">
+					<li className="py-1 px-6 bg-white text-[#004A99] font-bold">
+						Dashboard
+					</li>
+					<li>Settings</li>
+					<li>Help & FAQ</li>
+				</div>
+			</section>
+			<section>hello</section>
+		</div>
+	);
 }
